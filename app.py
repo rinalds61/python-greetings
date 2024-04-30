d39e432b1a10f9b31d1787fe499a7939fd7c7740
rinalds61 <166709342+rinalds61@users.noreply.github.com>
2024-04-30 11:04:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import argparse
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -8,5 +8,7 @@ def greetings():
     return jsonify({"greeting": "Hello from Python App!"})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 3001))  # Get port from environment variable, defaulting to 3000
-    app.run(host="0.0.0.0", debug=True, port=port)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=3000)
+    args = parser.parse_args()
+    app.run(host="0.0.0.0", debug=True, port=args.port)
