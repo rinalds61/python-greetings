feb48de2de87dee63330601fa570fbb7be197f4e
rinalds61 <166709342+rinalds61@users.noreply.github.com>
2024-04-30 13:49:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def greetings():
     return jsonify({"greeting": "Hello from Python App!"})
 
 if __name__ == '__main__':
-    port1 = int(os.environ.get("PORT", 3000))
+    port1 = os.environ.get("PORT", "3000")
     app.run(host="0.0.0.0", debug=True, port=port1)
