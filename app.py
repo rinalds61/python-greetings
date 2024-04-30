f7c200f203611bf7363609da5de23380762d4026
rinalds61 <166709342+rinalds61@users.noreply.github.com>
2024-04-30 15:57:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def greetings():
 
 if __name__ == '__main__':
     import argparse
-    port = int(os.environ.get('PORT', 5000))
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--port', default=5000, type=int, help='Port to run the server on.')
-    args = parser.parse_args()
+    port1 = int(os.environ.get('PORT', 5000))
+   # parser = argparse.ArgumentParser()
+    #parser.add_argument('--port', default=5000, type=int, help='Port to run the server on.')
+    #args = parser.parse_args()
     
-    app.run(debug=True, host='0.0.0.0', port=args.port)
+    app.run(debug=True, host='0.0.0.0', port=port1)
